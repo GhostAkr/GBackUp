8ececafeda4c5e762293ab34976c7401459f9675
Alexander Antoshkin <ighost99@gmail.com>
2023-09-17 18:16:16 +0300

    Using relative path of os_interaction folder

--- gbackup.sh
+++ gbackup.sh
@@ -1,5 +1,10 @@
+# TODO: These are mockup vars. Need to be removed when MVP is ready
 readonly EXT_VOL_NAME=GABackUp
 readonly RES_FILE_PATH=~/Desktop
+
+# Paths
+readonly SCRIPT_PATH=$(dirname $(realpath $0))
+readonly OS_INTERACTIONS_PATH=$SCRIPT_PATH/os_interaction
 
 # TODO: Execute only when an external drive with specific name is mounted
 # if [[ ${mount | awk '$3 == "/Volumes/${EXT_VOL_NAME}" {print $3}'} != "" ]];
@@ -7,7 +12,7 @@ if true;
 then
     dialog_text="Wanna see the rabit hole?"
 
-    res=$(osascript os_interaction/show_dialog.js "${dialog_text}")
+    res=$(osascript ${OS_INTERACTIONS_PATH}/show_dialog.js "${dialog_text}")
 
     case ${res} in
         YES)
