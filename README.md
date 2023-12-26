058465f589b59e22bb39aa5fa1b679d840388d6e
Alexander Antoshkin <ighost99@gmail.com>
2023-12-26 03:11:26 +0300

    Updated deploy.sh to deploy service

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GBackUp
-A simple Mac OS tool allowing to setup automated backup on external drive
+A simple Mac OS tool allowing to setup automated backup to an external drive.
+
+## Installation
+
+### Mac OS service
+
+First, deploy Mac OS service which will react on external drive mounting.
+
+1. Set "Configuration globals" variables in `deploy.sh`.
+2. Set path to `gbackup.sh` in "Program arguments" in `local.ghostakr.gbackup.plist` according to the home path in `deploy.sh`. `gbackup.sh` should be located at `~/Scripts`.
+3. Run `deploy.sh`.
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,23 @@
-readonly GBACKUP_SCRIPT=gbackup.sh
-readonly OS_INTERACTION_FOLDER=os_interaction
+# == Configuration globals ==
+# Set these globals to reflect your system
 
-readonly SCRIPT_TARGET_PATH=/Users/ghostakr/Scripts/
+# Home directory
+readonly HOME_PATH="/Users/ghostakr"
 
-cp $GBACKUP_SCRIPT $SCRIPT_TARGET_PATH
-cp -R $OS_INTERACTION_FOLDER $SCRIPT_TARGET_PATH
+# == Global paths ==
+
+readonly GBACKUP_SCRIPT="gbackup.sh"
+readonly SERVICE="local.ghostakr.gbackup.plist"
+
+readonly SCRIPT_TARGET_PATH="${HOME_PATH}/Scripts"
+readonly SERVICE_TARGET_PATH="${HOME_PATH}/Library/LaunchAgents"
+
+# == Copy files ==
+
+cp ${GBACKUP_SCRIPT} ${SCRIPT_TARGET_PATH}
+cp ${SERVICE} ${SERVICE_TARGET_PATH}
+
+# == Load services ==
+
+launchctl unload -w "${SERVICE_TARGET_PATH}/${SERVICE}" 2> /dev/null
+launchctl load -w "${SERVICE_TARGET_PATH}/${SERVICE}"
