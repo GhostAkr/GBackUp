7b438e691e364ea40d135c5db39a00d6165d2df1
Alexander Antoshkin <ighost99@gmail.com>
2023-12-26 19:04:53 +0300

    Using system $HOME in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,10 @@
-# == Configuration globals ==
-# Set these globals to reflect your system
-
-# Home directory
-readonly HOME_PATH="/Users/ghostakr"
-
 # == Global paths ==
 
 readonly GBACKUP_SCRIPT="gbackup.sh"
 readonly SERVICE="local.ghostakr.gbackup.plist"
 
-readonly SCRIPT_TARGET_PATH="${HOME_PATH}/Scripts"
-readonly SERVICE_TARGET_PATH="${HOME_PATH}/Library/LaunchAgents"
+readonly SCRIPT_TARGET_PATH="${HOME}/Scripts"
+readonly SERVICE_TARGET_PATH="${HOME}/Library/LaunchAgents"
 
 # == Copy files ==
 
