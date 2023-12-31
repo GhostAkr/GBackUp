a490a7cf513bbd761b385ed90507f5d5ab0ce5f3
Alexander Antoshkin <ighost99@gmail.com>
2023-12-31 20:19:08 +0300

    Style changes in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,13 @@ readonly SERVICE="local.ghostakr.gbackup.plist"
 readonly SCRIPT_TARGET_PATH="${HOME}/Scripts"
 readonly SERVICE_TARGET_PATH="${HOME}/Library/LaunchAgents"
 
-# == Copy files ==
+# == Deploy services ==
+
+cp ${SERVICE} ${SERVICE_TARGET_PATH}
+
+# == Deploy shell scripts ==
 
 cp ${GBACKUP_SCRIPT} ${SCRIPT_TARGET_PATH}
-cp ${SERVICE} ${SERVICE_TARGET_PATH}
 
 # == Load services ==
 
