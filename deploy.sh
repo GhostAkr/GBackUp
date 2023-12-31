32be768d0dc1c9b5803e91c1139ea6bfb05a5dbc
Alexander Antoshkin <ighost99@gmail.com>
2024-01-01 02:21:27 +0300

    Deploy library in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 
 readonly GBACKUP_SCRIPT="shell/gbackup.sh"
 readonly SERVICE="local.ghostakr.gbackup.plist"
+readonly LIBRARY="gbackup/gbackup_lib.js"
 
 readonly SCRIPT_TARGET_PATH="${HOME}/Scripts"
 readonly SERVICE_TARGET_PATH="${HOME}/Library/LaunchAgents"
+readonly LIBRARY_TARGET_PATH="${HOME}/Library/Script Libraries"
 
 # == Deploy services ==
 
@@ -13,6 +15,10 @@ cp ${SERVICE} ${SERVICE_TARGET_PATH}
 # == Deploy shell scripts ==
 
 cp ${GBACKUP_SCRIPT} ${SCRIPT_TARGET_PATH}
+
+# == Deploy library ==
+
+cp ${LIBRARY} "${LIBRARY_TARGET_PATH}"
 
 # == Load services ==
 
