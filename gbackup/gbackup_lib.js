7bd69739f78738cffc423afcb6adc49a6f237674
Alexander Antoshkin <ighost99@gmail.com>
2023-12-31 23:23:34 +0300

    Added searchForDrive to lib

--- gbackup/gbackup_lib.js
+++ gbackup/gbackup_lib.js
@@ -40,3 +40,47 @@ function getMountedDrives()
     
     return (res == "" ? falseRet : res)
 }
+
+/**
+ * Compares the list of given drives with the config.
+ * @param {string} drives List of drives to be compared in form of string separated with
+ * @returns {string} List of matched drives, "FALSE" if no drive is matched.
+ */
+function searchForDrive(drives)
+{
+    let app = Application.currentApplication()
+    app.includeStandardAdditions = true
+
+    let falseRet = "FALSE"
+    try
+    {
+        const libraryPath = app.pathTo("library folder", { from: "user domain" }).toString()
+        const configPath = String(`${libraryPath}/Preferences/GBackUp/config.json`)
+
+        let configStr = app.read(Path(configPath))
+        let config = JSON.parse(configStr)
+
+        let drivesList = drives.split(/\r/)
+        for (let ix = 0; ix < drivesList.length; ix++)
+            drivesList[ix] = drivesList[ix].split("/")[2]
+
+        let resDrives = []
+        drivesList.forEach(driveToCheck => {
+            config.drives.forEach(driveFromConfigRaw => {
+                let driveFromConfig = driveFromConfigRaw.name
+
+                if (driveToCheck.localeCompare(driveFromConfig, undefined,
+                    {sensitivity: "base"}) == 0
+                ){
+                    resDrives.push(driveToCheck)
+                }
+            })
+        })
+
+        return (resDrives.length == 0 ? falseRet : resDrives.toString())
+    }
+    catch
+    {
+        return falseRet
+    }
+}
