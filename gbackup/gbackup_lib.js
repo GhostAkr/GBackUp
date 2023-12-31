f52a4b8de9ae587f7d29766a93a3040a10be4595
Alexander Antoshkin <ighost99@gmail.com>
2023-12-31 21:46:35 +0300

    Added getMountedDrives() to lib

--- gbackup/gbackup_lib.js
+++ gbackup/gbackup_lib.js
@@ -15,3 +15,19 @@ function showNotification(text)
         soundName: "Frog"
     })
 }
+
+/**
+ * Obtains a list of drives monuted in the system. "Mounted" drives are the ones located at
+ * /Volumes.
+ * @returns {string} List of mounted drives, "FALSE" if no drive is mounted.
+ */
+function getMountedDrives()
+{
+    let app = Application.currentApplication()
+    app.includeStandardAdditions = true
+
+    let cmd = "mount | awk '$3 ~ \"^/Volumes/.*\" {print($3)}'"
+    let res = app.doShellScript(cmd)
+    
+    return res
+}
