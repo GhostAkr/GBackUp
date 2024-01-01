bf0447e7eedcc4c0c909f0291580319aa4415d32
Alexander Antoshkin <ighost99@gmail.com>
2024-01-01 16:20:13 +0300

    Added progress bar routines to the app component

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ First, deploy Mac OS service which will react on external drive mounting.
 1. Set path to `gbackup.sh` in "Program arguments" in `local.ghostakr.gbackup.plist` 
 according to the home path in `deploy.sh`. `gbackup.sh` should be located at `~/Scripts`.
 3. Run `deploy.sh`.
+
+## Debugging
+
+To debug GBackUp app copy the code from `gbackup.js` to a Script Editor in Mac OS. Then compile the application.
+
+**Note**: JXA application compiler from AppleScript VS Code extension doesn't work properly with external libraries.
--- gbackup/gbackup.js
+++ gbackup/gbackup.js
@@ -2,14 +2,55 @@ var app = Application.currentApplication()
 app.includeStandardAdditions = true
 
 /**
+ * Sets GBackUp app progress bar.
+ * @param {number} stepsCount Total number of steps for the progress bar.
+ */
+function setProgressBar(stepsCount)
+{
+    Progress.totalUnitCount = stepsCount
+    Progress.completedUnitCount = 0
+    Progress.description = "Performing backup..."
+    Progress.additionalDescription = "Preparing to backup."
+}
+
+/**
+ * Updates GBackUp app progress bar.
+ * @param {number} currentStep Current step for the progress bar.
+ */
+function updateProgressBar(currentStep)
+{
+    Progress.additionalDescription = "Processing " + currentStep + " out of " + 
+        Progress.totalUnitCount
+
+    Progress.completedUnitCount = currentStep
+}
+
+/**
+ * Unsets GBackUp app progress bar.
+ */
+function unsetProgressBar()
+{
+    Progress.totalUnitCount = 0
+    Progress.completedUnitCount = 0
+    Progress.description = ""
+    Progress.additionalDescription = ""
+}
+
+/**
  * GBackUp App entry point.
  * @param {} argv Ignored.
  */
 function run(argv)
 {
-    app.displayNotification("Hello, world!",
+    let gbackup_lib = Library("gbackup_lib.js")
+    gbackup_lib.showNotification("Test")
+
+    let stepsCount = 10
+    setProgressBar(stepsCount)
+    for (let i = 0; i < stepsCount; i++)
     {
-        withTitle: "GBackUp",
-        soundName: "Frog"
-    })
+        updateProgressBar(i + 1)
+        delay(1)
+    }
+    unsetProgressBar()
 }
